2765e561fcc05ef2117f2482d31e922861777ac2
Jesus Daniel Reyes Medina <reyesjd@uninorte.edu.co>
2021-05-05 21:08:10 -0500

    Connection to db created

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+//settings
+app.set("port", process.env.PORT || 3000);
+
+//middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+//databases conections
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB."))
+  .catch((err) => console.log(err));
+
+//routes
+app.get("/api",(req,res)=>{
+    res.json({message: "Hello World!"});
+});
+
+//Start Server
+app.listen(app.get("port"), () => {
+    console.log("Server on port ", app.get("port"));
+});
